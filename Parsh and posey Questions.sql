78cec72fc5824797c4d4a737ec9475e38c2e381c
M3tz <107323458+M3tz43@users.noreply.github.com>
2024-01-25 23:07:06 +0100

    Update Parsh and posey Questions.sql

--- Parsh and posey Questions.sql
+++ Parsh and posey Questions.sql
@@ -1,4 +1,3 @@
---JOIN QUERIES--
 
 /*Provide a table that provides the region for each sales_rep along with their associated accounts.
 This time only for the Midwest region. Your final table should include three columns: the region name, the sales rep name, and the account name.
